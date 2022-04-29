54ba202c686b7cdded14914226e8d7d03bb21b72
tedtalksbits <tedaneblake@gmail.com>
2022-04-28 20:34:24 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Accessing Data with JPA
+
+## Motive:
+Go through the processd of building an application that uses Spring Data JPA to store and retrieve data in relational database.
+
+## Tools used 
+
+- JDK 18
+- Maven
---
+++ src/test/java/com/tedaneblake/AccessingDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tedaneblake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessingDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
